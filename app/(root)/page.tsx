827ad00fe9c6dd9af4c9768c27db58f4f684ceae
mavasquez401 <mavasquez401@gmail.com>
2024-06-15 22:53:36 -0400

    version with error data wont connect locally

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client';
+
+import React from "react";
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "Manny" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access, manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
